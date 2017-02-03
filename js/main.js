730492d018208f8bd7dfc9ff61771383f0277ffd
Michael Reynolds <michael4reynolds@gmail.com>
2017-02-03 15:34:10 -0500

    add tweet and onload functions

--- js/main.js
+++ js/main.js
@@ -1,32 +1,38 @@
 const quoteButton = document.getElementById('quote-button');
 const quoteBox = document.getElementById('quote-box');
+const twitterBtn = document.getElementById('twitter-share');
 
-quoteButton.onclick = event => {
-  event.preventDefault();
-  axios.post('https://andruxnet-random-famous-quotes.p.mashape.com', {},
-    {
-      headers: {
-        "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Accept": "application/json"
-      },
-      params: {cat: 'movies'}
+const loadQuote = event => {
+  if (event) {
+    event.preventDefault();
+  }
+  axios.post('https://andruxnet-random-famous-quotes.p.mashape.com', {}, {
+    headers: {
+      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json"
+    },
+      params: {
+        cat: 'movies'
+      }
     })
     .then(function (response) {
       const {author, quote, category} = response.data;
-      quoteBox.innerHTML =
-          `<p class="has-text-left">${quote}</p>
+      quoteBox.innerHTML = `<p class="has-text-left">${quote}</p>
            <p class="has-text-right">
              <small id="quote-source">${category}</small>
            </p>
            <p class="has-text-right">
              <small id="quote-source">${author}</small>
            </p>`
+      twitterBtn.href = `https://twitter.com/intent/tweet?text=${quote} -- ${author}`
     })
     .catch(function (error) {
-      quoteBox.innerHTML =
-        `<p class="notification is-danger">
+      quoteBox.innerHTML = `<p class="notification is-danger">
           <button class="delete"></button>
         ${error}</p>`;
     });
 };
+
+quoteBox.onload = loadQuote();
+quoteButton.onclick = loadQuote;
